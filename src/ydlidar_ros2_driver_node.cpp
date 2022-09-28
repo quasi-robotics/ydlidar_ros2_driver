3a00e17fbbe0b7ed5425cbc9e924182c6a6f4b02
Fedor Bokov <fedor@quasi.ai>
2022-09-28 11:28:07 -0400

    fixed distance correction via parameters

--- src/ydlidar_ros2_driver_node.cpp
+++ src/ydlidar_ros2_driver_node.cpp
@@ -209,7 +209,9 @@ int main(int argc, char *argv[]) {
       for(size_t i=0; i < scan.points.size(); i++) {
         int index = std::ceil((scan.points[i].angle - scan.config.min_angle)/scan.config.angle_increment);
         if(index >=0 && index < size) {
-          scan_msg->ranges[index] = scan.points[i].range * correction_ratio + correction_offset;
+          scan_msg->ranges[index] = scan.points[i].range;
+          if(std::isfinite(scan_msg->ranges[index]))
+            scan_msg->ranges[index] = scan_msg->ranges[index] * correction_ratio + correction_offset;
           scan_msg->intensities[index] = scan.points[i].intensity;
         }
       }
